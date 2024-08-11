d27c9e2d3645ba7b93c07c0d538923dc5a71f14d
Pandelis <pandelis777@gmail.com>
2024-08-11 10:17:52 +0300

    Added exercise 1.5

---
+++ part1/1.5/src/App.jsx
@@ -0,0 +1,64 @@
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const Content = (props) => {
+  console.log("🚀 ~ Content ~ props:", props);
+  return (
+    <>
+      {props.parts.map((item) => {
+        console.log("🚀 ~ {props.map ~ item:", item);
+        return (
+          <p key={item.name}>
+            {item.name} {item.exercises}
+          </p>
+        );
+      })}
+    </>
+  );
+};
+
+const Total = (props) => {
+  console.log("🚀 ~ Total ~ props:", props);
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {props.parts.reduce(
+          (accumulator, currentValue) => accumulator + currentValue.exercises,
+          0
+        )}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
