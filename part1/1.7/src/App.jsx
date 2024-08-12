7ddfe6afa848f22761ba2527ab7efd14bacce3d5
Pandelis <pandelis777@gmail.com>
2024-08-13 02:27:20 +0300

    Added exercise 1.7

---
+++ part1/1.7/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const FeedbackStat = ({ text, value }) => {
+  return (
+    <div>
+      {text}: {value}
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  const total = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / total;
+  const positive = ((good / total) * 100).toFixed(2) + "%";
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Button text="good" onClick={handleGoodClick} />
+      <Button text="neutral" onClick={handleNeutralClick} />
+      <Button text="bad" onClick={handleBadClick} />
+
+      <h2>Statistics</h2>
+      <FeedbackStat text="good" value={good} />
+      <FeedbackStat text="neutral" value={neutral} />
+      <FeedbackStat text="bad" value={bad} />
+      <FeedbackStat text="total" value={total} />
+      <FeedbackStat text="average" value={average} />
+      <FeedbackStat text="positive" value={positive} />
+    </div>
+  );
+};
+
+export default App;
