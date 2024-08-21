9a6a19cd8e67dd4e1dbbfcdbed0ed4c9ba7b023f
Pandelis <pandelis777@gmail.com>
2024-08-21 19:35:51 +0300

    Added exercise 2.18 completed

--- part2/2.18-2.19-2.20/countries/src/components/Countries.jsx
+++ part2/2.18-2.19-2.20/countries/src/components/Countries.jsx
@@ -1,21 +1,48 @@
 const Countries = ({ filteredCountries }) => {
-	console.log("🚀 ~ Countries ~ filteredCountries:", filteredCountries);
-	const numCountries = filteredCountries.length;
+  console.log("🚀 ~ Countries ~ filteredCountries:", filteredCountries);
+  const numCountries = filteredCountries.length;
 
-	return (
-	  <div>
-		{numCountries > 10 ? (
-		  <p>Too many matches, specify another filter.</p>
-		) : (
-		  <ul>
-			{filteredCountries.map((country, index) => (
-			  <li key={index}>{country.name.common}</li>
-			))}
-		  </ul>
-		)}
-	  </div>
-	);
-  };
-  
-  export default Countries;
-  
+  return (
+    <div>
+      {numCountries > 10 ? (
+        <p>Too many matches, specify another filter.</p>
+      ) : numCountries > 1 ? (
+        <ul>
+          {filteredCountries.map((country, index) => (
+            <li key={index}>{country.name.common}</li>
+          ))}
+        </ul>
+      ) : numCountries === 1 ? (
+        <div>
+          <h1>{filteredCountries[0].name.common}</h1>
+          <div>
+            <span>capital </span>
+            {filteredCountries[0].capital}
+          </div>
+          <div>
+            <span>area </span>
+            {filteredCountries[0].area}
+          </div>
+          <br />
+          <div>
+            <span style={{ fontWeight: 700 }}>Languages: </span>
+            <ul>
+              {Object.values(filteredCountries[0].languages).map(
+                (language, index) => (
+                  <li key={index}>{language}</li>
+                )
+              )}
+            </ul>
+          </div>
+		  <div>
+            <img src={filteredCountries[0].flags.png} alt="" style= {{ maxHeight: "180px"}}/>
+          </div>
+        </div>
+      ) : (
+        <p>No matches found.</p>
+      )}
+    </div>
+  );
+};
+
+export default Countries;
