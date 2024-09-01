5d6509f6b5e3e5e40806aa2ef37b05fe47665d34
PKond <pandelis777@gmail.com>
2024-09-01 18:46:29 +0300

    Remove from numbers.js the localhost

--- part3/3.11/frontend/src/services/numbers.js
+++ part3/3.11/frontend/src/services/numbers.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons/";
+const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -17,9 +17,9 @@ const update = (id, newObject) => {
 };
 
 const deletePerson = (id) => {
-	const request = axios.delete(`${baseUrl}/${id}`);
-	return request.then((response => response.data));
-}
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 
 export default {
   getAll,
